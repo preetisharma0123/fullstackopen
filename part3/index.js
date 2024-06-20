65741e0ff93d8efc9dc7f7134147383aa9f8bea9
Preeti Sharma <preetisharma0123@gmail.com>
2024-06-19 22:56:51 -0300

    3.6: Phonebook backend step 6

--- part3/index.js
+++ part3/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 let persons =[
   { 
@@ -24,10 +25,6 @@ let persons =[
   }
 ]
 
-let countPerson = persons.length
-var timeInMss = new Date();
-
-
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
@@ -37,11 +34,60 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/info', (request, response) => {
+  let countPerson = persons.length
+  var timeInMss = new Date();
   response.send(`<p>Phonebook has info for ${countPerson} people</p>
   <p>${timeInMss}</p>`)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
 
+  if (person){
+    response.json(person)
+  } else{
+    response.status(404).end()
+  }
+
+})
+
+app.delete('/api/persons/:id', (request,response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+  response.status(204).end()
+})
+
+const generateID = () => {
+  const maxID = persons.length >0
+    ? Math.max(...persons.map(person => person.id))
+    : 0
+    return maxID + 1
+}
+app.post('/api/persons', (request,response) => {
+  const body = request.body
+
+  const newName = persons.find(person => person.name === body.name)
+  const newNumber = persons.find(person => person.number === body.number)
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }else if(newName){
+    response.status(409).json({ error: 'name must be unique' })
+  }else if (newNumber){
+    response.status(409).json({ error: 'number must be unique' })
+  }else{
+      const newPerson = {
+      id: generateID(),
+      name: body.name,
+      number: body.number,
+  }
+    persons = persons.concat(newPerson)
+    response.json(newPerson)
+  }
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
