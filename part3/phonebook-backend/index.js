fa6f263bcede8e5c53077910af17d4e45487c32b
Preeti Sharma <preetisharma0123@gmail.com>
2024-07-10 15:45:26 -0300

    Add utils

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -4,11 +4,11 @@ const app = express();
 const morgan = require("morgan");
 const cors = require('cors');
 const Person = require('./models/person');
+const config = require('./utils/config');
+const logger = require('./utils/logger')
 
 app.use(express.static('dist'))
-
 app.use(cors())
-
 app.use(express.json());
 
 morgan.token("type", function (req, res) {
@@ -23,72 +23,117 @@ app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
+// get all persons
 app.get('/api/persons', (request, response) => {
-  Person.find({}).then(persons => {
-    response.json(persons)
+  Person.find({})
+  .then(persons => {
+    if (persons){
+      response.json(persons)
+    } else {
+      response.status(404).end()
+    }
   })
+  .catch(error => next(error))
 })
 
-app.get('/info', (request, response) => {
-  let countPerson = persons.length
-  var timeInMss = new Date();
-  response.send(`<p>Phonebook has info for ${countPerson} people</p>
-  <p>${timeInMss}</p>`)
-})
+app.get('/api/info', (request, response,next) => {
+  Person.find({})
+  .then(persons => {
+    let personCount = persons.length
+    var timeInMss = new Date();
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
+    if (personCount){
+      response.send(`<p>Phonebook has info for ${personCount} people</p>
+      <p>${timeInMss}</p>`)
+    }else{
+      response.status(400).end()
+     } 
+   })
+   .catch((error) => next(error))
   })
 
-  if (person){
-    response.json(person)
-  } else{
-    response.status(404).end()
-  }
 
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person){
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
+
 
 app.delete('/api/persons/:id', (request,response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-  response.status(204).end()
+    Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(200).end()
+    })
+    .catch(error => next(error))
 })
 
-const generateID = () => {
-  const maxID = persons.length >0
-    ? Math.max(...persons.map(person => person.id))
-    : 0
-    return maxID + 1
-}
+
 app.post('/api/persons', (request,response) => {
   const body = request.body
 
-  const newName = persons.find(person => person.name === body.name)
-  const newNumber = persons.find(person => person.number === body.number)
-
-  if (!body.name || !body.number) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
   }
-  // else if(newName){
-  //   response.status(409).json({ error: 'name must be unique' })
-  // }else if (newNumber){
-  //   response.status(409).json({ error: 'number must be unique' })
-  // }else{
-    const person = new Person({
-    id: generateID(),
-    name: body.name,
-    number: body.number,
+
+  Person.find({name: body.name})
+    .then(result => {
+      const person = {
+        name: result.name,
+        number: result.number,
+        }
+  
+      Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+          response.json(updatedPerson)
+        })
+        .catch(error => next(error))
     })
-    person.save().then(savedPerson => {
-      response.json(savedPerson)
+  
+  const newperson = new Person({
+  name: body.name,
+  number: body.number,
+  })
+  newperson.save().then(savedPerson => {
+    response.json(savedPerson)
+  }).catch((error) => next(error))
+})
+  
+app.put('/api/persons/:id', (request, response, next) => {
+  const {name,number} = request.body
+
+  Person.findByIdAndUpdate(request.params.id, 
+    {name,number},
+    { new: true, runValidators: true, context: 'query' }
+  )
+    .then(updatedPerson => {
+      response.json(updatedPerson)
     })
-   
+    .catch(error => next(error))
 })
 
-const PORT = process.env.PORT
+
+const errorHandler = (error, request, response, next) => {
+  logger.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  logger.info(`Server running on port ${config.PORT}`)
 })
