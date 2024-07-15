517f857b17ad1613193aa9c56e7fa012687ee994
Preeti Sharma <preetisharma0123@gmail.com>
2024-07-14 23:35:34 -0300

    Add part4 - blogs

---
+++ part4/blogs/index.js
@@ -0,0 +1,10 @@
+const app = require('./App') // the actual Express application
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
+})
+
+app.use(cors())
+app.use(express.json())
