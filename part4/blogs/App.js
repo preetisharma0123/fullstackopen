b6fca7eabc2b3cbfcef4442dafda489edafe1962
Preeti Sharma <preetisharma0123@gmail.com>
2024-07-15 23:52:53 -0300

    corrections

--- part4/blogs/App.js
+++ part4/blogs/App.js
@@ -1,9 +1,10 @@
-const config = require('./utils/config')
 const express = require('express')
 const app = express()
+const config = require('./utils/config')
 const cors = require('cors')
-const blogsRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
+const blogsRouter = require('./controllers/blogs')
+
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
