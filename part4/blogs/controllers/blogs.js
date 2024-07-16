7d21256b3757fad7ca735cebf9c40d449074dbcf
Preeti Sharma <preetisharma0123@gmail.com>
2024-07-15 23:03:45 -0300

    corrections

--- part4/blogs/controllers/blogs.js
+++ part4/blogs/controllers/blogs.js
@@ -1,15 +1,32 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-app.get('/', (request, response) => {
+blogsRouter.get('/', (request, response) => {
     Blog
       .find({})
       .then(blogs => {
         response.json(blogs)
       })
   })
+
+  blogsRouter.get('/:id', (request, response, next) => {
+    Note.findById(request.params.id)
+      .then(note => {
+        if (note) {
+          response.json(note)
+        } else {
+          response.status(404).end()
+        }
+      })
+      .catch(error => next(error))
+  })
   
-  app.post('/', (request, response) => {
+  
+  blogsRouter.post('/', (request, response, next) => {
+    const body = request.body
+    if (body.content === undefined) {
+      return response.status(400).json({ error: 'content missing' })
+  }
     const blog = new Blog(request.body)
   
     blog
@@ -17,6 +34,7 @@ app.get('/', (request, response) => {
       .then(result => {
         response.status(201).json(result)
       })
+      .catch(error => next(error))
   })
 
   module.exports = blogsRouter
--- part4/blogs/index.js
+++ part4/blogs/index.js
@@ -1,10 +1,7 @@
-const app = require('./App') // the actual Express application
+const app = require('./app') // the actual Express application
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
-
-app.use(cors())
-app.use(express.json())
