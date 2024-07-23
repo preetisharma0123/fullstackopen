7c3aca952b6192e19972047a6ddffbf2aa7bb8f3
Preeti Sharma <preetisharma0123@gmail.com>
2024-07-22 23:34:17 -0300

    Add favoriteBlog helper function

--- part4/blogs/utils/list_helper.js
+++ part4/blogs/utils/list_helper.js
@@ -14,6 +14,23 @@ const totalLikes = (blogs) => {
   return blogs.reduce(sumOfLikes, 0);
 }
 
+// define function favoriteBlog
+const favoriteBlog = (blogs) => {
+  // receive an array of blogs as a parameter
+  // return the blog with the most likes
+  const mostLikedBlog = blogs.reduce((max, blog) => {
+    return blog.likes > max.likes ? blog : max;
+  }, blogs[0]);
+
+  const blogData = {
+    title: mostLikedBlog.title,
+    author: mostLikedBlog.author,
+    likes: mostLikedBlog.likes,
+  };
+
+  return blogData;
+};
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
