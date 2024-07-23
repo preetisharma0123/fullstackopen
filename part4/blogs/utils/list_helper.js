426194ed60e7e2d9a25678d5768fbf6e639abe79
Preeti Sharma <preetisharma0123@gmail.com>
2024-07-23 00:03:48 -0300

    list_helper.test functions add

--- part4/blogs/utils/list_helper.js
+++ part4/blogs/utils/list_helper.js
@@ -7,11 +7,7 @@ const totalLikes = (blogs) => {
   // return the total sum of likes in all of the blog posts
   // if the array is empty, return 0
 
-  const sumOfLikes = (sum, blog) => {
-    return sum + blog.likes
-  }
-
-  return blogs.reduce(sumOfLikes, 0);
+  return blogs.reduce((sum, blog)=> sum + blog.likes, 0);
 }
 
 // define function favoriteBlog
